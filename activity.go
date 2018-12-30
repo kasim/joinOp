21e95604ceea3209fba41079e2ea29542654f7ab
Steven Chu <chu.chongmeng@gmail.com>
2018-12-30 17:18:14 +0800

    Changed the button one to global cache so that it has purge time and expiry time.

--- activity.go
+++ activity.go
@@ -2,12 +2,19 @@ package joinOp
 
 import (
 	"fmt"
+	"strconv"
+
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/core/data"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
+	cache "github.com/patrickmn/go-cache"
 )
 
 var log = logger.GetLogger("activity-joinOp")
+
+const (
+	CacheName = "GlobalCache"
+)
 
 // MyActivity is a stub for your Activity implementation
 type MyActivity struct {
@@ -31,9 +38,25 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
 	input1Nm := context.GetInput("input1").(string)
 	input2Nm := context.GetInput("input2").(string)
 
-	output1, ok := data.GetGlobalScope().GetAttr(input1Nm)
+	var c *cache.Cache
+	val, ok := data.GetGlobalScope().GetAttr(CacheName)
+
 	if !ok {
+		log.Error("cache doesn't exist")
+		return false, fmt.Errorf("cache doesn't exist")
+	} else {
+		c = val.Value().(*cache.Cache)
+	}
+	cacheVal, found := get(c, input1Nm)
+	if !found {
+		log.Infof("No cache entry was found for [%s]", input1Nm)
 		errorMsg := fmt.Sprintf("Attribute not defined: '%s'", input1Nm)
+		log.Error(errorMsg)
+		return false, activity.NewError(errorMsg, "", nil)
+	}
+	output1, err := strconv.ParseBool(cacheVal.(string))
+	if err != nil {
+		errorMsg := fmt.Sprintf("Attribute '%s' is not in right content: '%s'", input1Nm, cacheVal)
 		log.Error(errorMsg)
 		return false, activity.NewError(errorMsg, "", nil)
 	}
@@ -44,7 +67,12 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
 		return false, activity.NewError(errorMsg, "", nil)
 	}
 
-	output := (output1.Value().(bool) && output2.Value().(bool))
+	output := (output1 && output2.Value().(bool))
 	context.SetOutput("output", output)
 	return true, nil
+}
+
+func get(c *cache.Cache, key string) (interface{}, bool) {
+		return c.Get(key)
+
 }
--- activity_test.go
+++ activity_test.go
@@ -3,11 +3,13 @@ package joinOp
 import (
 	"io/ioutil"
 	"testing"
+	"time"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
 	"github.com/TIBCOSoftware/flogo-lib/core/data"
 	"github.com/stretchr/testify/assert"
+	cache "github.com/patrickmn/go-cache"
 )
 
 var activityMetadata *activity.Metadata
@@ -53,6 +55,9 @@ func TestEval(t *testing.T) {
 	tc.SetInput("input1", "b1")
 	tc.SetInput("input2", "avgTemp")
 	dt, _ := data.ToTypeEnum("bool")
+	c := cache.New(time.Duration(1)*time.Minute, time.Duration(1)*time.Minute)
+	data.GetGlobalScope().AddAttr("GlobalCache", data.TypeAny, c)
+	set(c, "b1", "true")
 	data.GetGlobalScope().AddAttr("b1", dt, true)
 	data.GetGlobalScope().AddAttr("avgTemp", dt, true)
 	act.Eval(tc)
@@ -61,4 +66,8 @@ func TestEval(t *testing.T) {
 
 	assert.Equal(t, output, true)
 	//check result attr
+}
+
+func set(c *cache.Cache, key string, value string) {
+		c.Set(key, value, cache.DefaultExpiration)
 }
